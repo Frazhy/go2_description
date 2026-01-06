fc2e18d7c42e981a83410b134c4c88aef9f55d4b
Frazhy <franky.quispe.d@uni.pe>
2026-01-07 00:48:23 +0800

    Sim Real

--- launch/description_real.launch.py
+++ launch/description_real.launch.py
@@ -13,6 +13,7 @@ def generate_launch_description():
 
     pkg_share = FindPackageShare("go2_description").find("go2_description")
     default_model_path = os.path.join(pkg_share, "xacro", "robot.xacro")
+    rviz_config_path = os.path.join(pkg_share, "rviz", "go2.rviz")
 
     declare_use_sim_time = DeclareLaunchArgument(
         "use_sim_time", default_value="false"
@@ -26,13 +27,36 @@ def generate_launch_description():
         executable="robot_state_publisher",
         output="screen",
         parameters=[
-            {"robot_description": Command(["xacro", " ", description_path])},
+            {
+                "robot_description": Command([
+                    os.path.expanduser("~/.local/bin/xacro"),
+                    " ",
+                    description_path
+                ])
+            },
             {"use_sim_time": use_sim_time},
         ],
+    )
+    
+    topic_node = Node(
+        package="go2_state_bridge",
+        executable="lowstate_to_jointstate",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}],  
+    )
+    
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_path],
+        parameters=[{"use_sim_time": use_sim_time}],
     )
 
     return LaunchDescription([
         declare_use_sim_time,
         declare_description_path,
+        topic_node,
         robot_state_publisher_node,
+        rviz_node,
     ])
